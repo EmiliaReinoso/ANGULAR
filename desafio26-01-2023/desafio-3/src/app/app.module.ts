d52285c1b0bfff054c82413ee4abac90bbcbe9af
Emilia Reinoso <emilia.reinoso@gmail.com>
2023-02-04 15:28:20 -0300

    04-02-2023-Creacion-componente-form

--- desafio26-01-2023/desafio-3/src/app/app.module.ts
+++ desafio26-01-2023/desafio-3/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentePadreComponent } from './components/componente-padre/componente-padre.component';
+import { ComponenteHijoComponent } from './components/componente-hijo/componente-hijo.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentePadreComponent,
+    ComponenteHijoComponent,
+    FormularioReactivoComponent
   ],
   imports: [
     BrowserModule
